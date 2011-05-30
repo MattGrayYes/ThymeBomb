3e971e307ec773917e71fe23e34c3e162166bd2b
Matt Gray <git@mattg.co.uk>
2011-05-30 13:07:33 +0100

    Added skeleton Menu Controller to project

---
+++ ThymeBomb/ThymeBombMenuController.h
@@ -0,0 +1,17 @@
+//
+//  ThymeBombMenuController.h
+//  ThymeBomb
+//
+//  Created by Matt Gray on 30/05/2011.
+//  Copyright 2011 mattg.co.uk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ThymeBombMenuController : NSMenu {
+@private
+    
+}
+
+@end
---
+++ ThymeBomb/ThymeBombMenuController.m
@@ -0,0 +1,29 @@
+//
+//  ThymeBombMenuController.m
+//  ThymeBomb
+//
+//  Created by Matt Gray on 30/05/2011.
+//  Copyright 2011 mattg.co.uk. All rights reserved.
+//
+
+#import "ThymeBombMenuController.h"
+
+
+@implementation ThymeBombMenuController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
